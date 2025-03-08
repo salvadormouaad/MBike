3c68bf69d1d00fff2169737349ac7f350d564d52
mouad <osfourmouad32@gmail.com>
2025-03-08 22:04:57 +0000

    'BikesSlice'

---
+++ MBike/src/Bikes/SliceBikes/BikesPic/Bikes.jsx
@@ -0,0 +1,25 @@
+import Agree1 from './Agree1'
+import Agree2 from './Agree2'
+import Agree3 from './Agree3'
+import Ams1 from './Ams1'
+import Ams2 from './Ams2'
+import Ams3 from './Ams3'
+import Hyde1 from './Hyde1'
+import Hyde2 from './Hyde2'
+import Hyde3 from './Hyde3'
+
+export const Agree = {
+    Agree1,
+    Agree2,
+    Agree3,
+}
+export const Ams = {
+    Ams1,
+    Ams2,
+    Ams3,
+}
+export const Hyde = {
+    Hyde1,
+    Hyde2,
+    Hyde3,
+}
