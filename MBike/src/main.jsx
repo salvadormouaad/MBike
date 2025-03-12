521594688019322d664ca8846ec99215ee23161d
anas na <an.na@gmail.com>
2025-03-12 10:47:54 +0000

    devlopement 4 slice gear

--- MBike/src/main.jsx
+++ MBike/src/main.jsx
@@ -3,12 +3,21 @@ import './index.css'
 //import App from './App'
 import { Provider } from 'react-redux';
 import { StoreGlobal } from './store/storeGlobal';
-import Index1 from './index1';
-import Buttonn from './index3';
+import Carde1 from './carde1';
+import Carde2 from './carde2';
+import Carde3 from './carde3';
+import Carde4 from './carde4';
+import Carde5 from './carde5';
+import Carde6 from './carde6';
+import Carde7 from './carde7';
+import Carde8 from './carde8';
+import Carde9 from './carde9';
+import Carde10 from './carde10';
+import TrustedBrands from './TrustedBrands';
 
 
 createRoot(document.getElementById('root')).render(
     <Provider store={StoreGlobal}>
-        <Index1 />
+        <TrustedBrands/>
     </Provider>
 )
