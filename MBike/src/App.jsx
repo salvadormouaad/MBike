41c79d8f7d21b93bd284b5d4cac6db7884b7c394
Abdelilah <elhasnaouiabdelilah8@gmail.com>
2025-03-26 00:19:39 +0000

    Add files via upload

---
+++ MBike/src/App.jsx
@@ -0,0 +1,12 @@
+import { RouterProvider } from "react-router-dom";
+import { Router } from "./Routes/Route";
+
+function App() {
+    return (
+        <>
+            <RouterProvider router={Router} />
+        </>
+    );
+}
+
+export default App;
