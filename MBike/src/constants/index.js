5ac3de839494cd1057af0973ddb75ba5127401c2
Abdelilah <elhasnaouiabdelilah8@gmail.com>
2025-03-26 00:08:41 +0000

    Add files via upload

---
+++ MBike/src/constants/index.js
@@ -0,0 +1,57 @@
+export const navLists = [
+    { name: "Home", path: "/" },
+    { name: "Bikes", path: "/bikes" },
+    { name: "Gear", path: "/gears" },
+    { name: "Catalog", path: "/catalog" },
+    { name: "About", path: "/about" },
+];
+
+import react from "../assets/images/react.svg";
+import hero from "../assets/images/bike background 2.png";
+import bag from "../assets/images/bag.svg";
+import search from "../assets/images/search.svg";
+import profile from "../assets/images/profile.svg";
+import menu from "../assets/images/menu.svg";
+import arrow from "../assets/images/arrow.svg";
+import close from "../assets/images/close.svg";
+import fox from "../assets/images/fox.svg";
+import cube from "../assets/images/cube.svg";
+import giant from "../assets/images/giant.svg";
+import scott from "../assets/images/scott.svg";
+import yamaha from "../assets/images/yamaha.svg";
+import bianchi from "../assets/images/bianchi.svg";
+import cervelo from "../assets/images/cervelo.svg";
+import colnago from "../assets/images/colnago.svg";
+import focus from "../assets/images/focus.svg";
+import fuji from "../assets/images/fuji.svg";
+import merida from "../assets/images/merida.svg";
+import pinarello from "../assets/images/pinarello.svg";
+import santa_cruz from "../assets/images/santa-cruz.svg";
+import bike3 from "../assets/images/bike3.jpeg";
+// importing  NavIcons
+export const logo = react;
+export const searchImg = search;
+export const bagImg = bag;
+export const profileImg = profile;
+export const heroImg = hero;
+export const menuIcon = menu;
+export const rightArrow = arrow;
+export const closeIcon = close;
+export const bikeSection = bike3;
+
+// brands logos
+export const brands = [
+    fox,
+    cube,
+    giant,
+    scott,
+    yamaha,
+    bianchi,
+    cervelo,
+    colnago,
+    focus,
+    fuji,
+    merida,
+    pinarello,
+    santa_cruz,
+];
